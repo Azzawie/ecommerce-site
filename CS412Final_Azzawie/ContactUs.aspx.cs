19cd9e412b4ad55db410c04f91f65338431418e2
mwmaki <mwmaki2@gmail.com>
2021-06-16 01:32:24 -0500

    Finish contact us page

--- CS412Final_Azzawie/ContactUs.aspx.cs
+++ CS412Final_Azzawie/ContactUs.aspx.cs
@@ -11,7 +11,51 @@ namespace CS412Final_Azzawie
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            // Don't show the errors panel when the page load.
+            errorsPanel.Visible = false;
+        }
 
+        protected void btnContactUs_Click(object sender, EventArgs e)
+        {
+
+            // initiale a list which will contain all the errors (if exist).
+            List<string> errors = new List<string>();
+
+            // Check if the name field is empty
+            if (string.IsNullOrEmpty(name.Text))
+            {
+                errors.Add("Please enter your name !");
+            }
+
+            // Check if the email field is empty
+            if (string.IsNullOrEmpty(email.Text))
+            {
+                errors.Add("Please enter a valid email !");
+            }
+
+            // Check if the phone field is empty
+            if (string.IsNullOrEmpty(phone.Text))
+            {
+                errors.Add("Please enter a valid phone number !");
+            }
+
+            // Check if the comment field is empty
+            if (string.IsNullOrEmpty(comment.Text))
+            {
+                errors.Add("Please enter a brief message !");
+            }
+
+            // Display all errors if it's exist.
+            if (errors.Count > 0)
+            {
+                errorsPanel.Visible = true;
+                errorsPanel.BorderColor = System.Drawing.Color.Red;
+                errorsLbl.Text = string.Join("</br>", errors);
+                return;
+            }
+
+            // If there are no errors then we redirect to the home page.
+            Response.Redirect("./Home.aspx");
         }
     }
 }
--- CS412Final_Azzawie/ContactUs.aspx.designer.cs
+++ CS412Final_Azzawie/ContactUs.aspx.designer.cs
@@ -13,5 +13,77 @@ namespace CS412Final_Azzawie
 
     public partial class ContactUs
     {
+
+        /// <summary>
+        /// name control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox name;
+
+        /// <summary>
+        /// email control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox email;
+
+        /// <summary>
+        /// phone control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox phone;
+
+        /// <summary>
+        /// comment control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox comment;
+
+        /// <summary>
+        /// panelContactUs control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel panelContactUs;
+
+        /// <summary>
+        /// btnContactUs control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnContactUs;
+
+        /// <summary>
+        /// errorsPanel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel errorsPanel;
+
+        /// <summary>
+        /// errorsLbl control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label errorsLbl;
     }
 }
