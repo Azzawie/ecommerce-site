89348b665e6cd529bd18efad0b4654fdc0641c2d
mwmaki <mwmaki2@gmail.com>
2021-06-16 05:02:28 -0500

    Update the ad

--- CS412Final_Azzawie/Models/Ad.cs
+++ CS412Final_Azzawie/Models/Ad.cs
@@ -10,6 +10,9 @@ namespace CS412Final_Azzawie.Models
     {
         public long Id { get; set; }
         public string Title { get; set; }
+        public User User { get; set; }
+        
+        // I'll switch the type to be decimal later when we use the db
         public string Price { get; set; }
         public string Description { get; set; }
         public string Condition { get; set; }
