5afa8fabb566a35d260aed30acad028830807f35
mwmaki <mwmaki2@gmail.com>
2021-06-16 03:04:05 -0500

    Create new models

---
+++ CS412Final_Azzawie/Models/Address.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CS412Final_Azzawie.Models
+{
+    [Serializable]
+    public class Address
+    {
+        public long Id { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Street1 { get; set; }
+        public string Street2 { get; set; }
+        public string ZipCode { get; set; }
+        public string UnitNymber { get; set; }
+    }
+}
---
+++ CS412Final_Azzawie/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CS412Final_Azzawie.Models
+{
+    [Serializable]
+    public class User
+    {
+        public long Id { get; set; }
+        public string First { get; set; }
+        public string Last { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+        public DateTime Dob { get; set; }
+        public Address Address { get; set; }
+    }
+}
