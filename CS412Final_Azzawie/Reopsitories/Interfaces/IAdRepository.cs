f62cb0ef8181696543b43e3b5b9987218d9270b7
mwmaki <mwmaki2@gmail.com>
2021-06-25 21:46:46 -0500

    Create the BLL, Repositories and Interfaces files

---
+++ CS412Final_Azzawie/Reopsitories/Interfaces/IAdRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CS412Final_Azzawie.Reopsitories.Interfaces
+{
+    public class IAdRepository
+    {
+    }
+}
